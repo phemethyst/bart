9dacd0de217ea1cc5339708f5253211d4d8da1bf
phemethyst <phemethyst@proton.me>
2026-09-18 00:09:41 +0200

    basic goals for entity, now we eep :3

--- src/main/java/org/phemethyst/bart/entity/custom/BartEntity.java
+++ src/main/java/org/phemethyst/bart/entity/custom/BartEntity.java
@@ -2,14 +2,28 @@ package org.phemethyst.bart.entity.custom;
 
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-public class BartEntity extends Mob {
+public class BartEntity extends Monster {
     public static final AnimationState idleAnimState = new AnimationState();
     private int idleAnimTimeout = 0;
 
-    public BartEntity(EntityType<? extends Mob> entityType, Level level) {
+    // tutorial timestamp 7:05
+    // kaupenjoe my goat
+    // and if anyone has an issue, this is for bap and i kinda want something, right?
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 3.0, true));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, false));
+    }
+
+    public BartEntity(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
     }
 }
